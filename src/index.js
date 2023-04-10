a4f6d7ce69bc94bdbf56095f1eaa805f9c3cbbf7
Lalo Freak <lalofreak.dev@gmail.com>
2023-04-10 17:02:40 -0400

    thrash

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-      <React.StrictMode>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </React.StrictMode>
     </Provider>
 );
