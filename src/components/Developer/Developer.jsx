b16bd3c9fa88b8af4bc12e5b64dbf224ed9af481
Lalo Freak <lalofreak.dev@gmail.com>
2023-04-17 09:32:08 -0400

    thrash

--- src/components/Developer/Developer.jsx
+++ src/components/Developer/Developer.jsx
@@ -3,6 +3,9 @@ import { OptionTitle } from "../Options/OptionTitle"
 import s from "./css/Developer.module.css"
 import { useEffect, useState } from "react"
 import { getProgramming } from "../../middlewares/redux/actions"
+import webIcon from "../../images/web-icon-black.png"
+import desktopIcon from "../../images/desktop-icon.png"
+import mobileIcon from "../../images/mobile-icon.png"
 
 export const Developer = () => {
   const language = useSelector(state=>state.language)
@@ -21,9 +24,10 @@ export const Developer = () => {
       <OptionTitle title={language==='EN'? 'portfolio: programming':'portafolio: programacion'}/>
       <div className={s.works}>
         <ul className={s.devUl}>
-          <li key={'title1'} className={s.devLi}>
-            <h1 onClick={()=> {shownState1==="none"? setShownState1("flex") : setShownState1("none")}}>
+          <li key={'title1'} className={s.devLi} >
+            <h1 style={{display: "flex", justifyContent:"space-between"}} onClick={()=> {shownState1==="none"? setShownState1("flex") : setShownState1("none")}}>
               Web
+              <img style={{marginRight: "50px"}} src={webIcon} width="40px" height="40px" alt="web" />
             </h1>
           </li>
           {
@@ -50,8 +54,9 @@ export const Developer = () => {
         </ul>
         <ul className={s.devUl}>
           <li key={'title2'} className={s.devLi}>
-            <h1 onClick={()=> {shownState2==="none"? setShownState2("flex") : setShownState2("none")}}>
+            <h1 style={{display: "flex", justifyContent:"space-between"}} onClick={()=> {shownState2==="none"? setShownState2("flex") : setShownState2("none")}}>
               {language==='EN'?'Desktop':'Escritorio'}
+              <img style={{marginRight: "50px"}} src={desktopIcon} width="40px" height="40px" alt="desktop" />
             </h1>
           </li>
           {
@@ -78,8 +83,9 @@ export const Developer = () => {
         </ul>
         <ul className={s.devUl}>
           <li key={'title3'} className={s.devLi}>
-            <h1 onClick={()=> {shownState3==="none"? setShownState3("flex") : setShownState3("none")}}>
+            <h1 style={{display: "flex", justifyContent:"space-between"}} onClick={()=> {shownState3==="none"? setShownState3("flex") : setShownState3("none")}}>
               {language==='EN'?'Mobile': 'Movil'}
+              <img style={{marginRight: "50px"}} src={mobileIcon} width="40px" height="40px" alt="mobile" />
             </h1>
           </li>
           {
