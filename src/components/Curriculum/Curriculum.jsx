d11d77d1ca55b1e9fb233345504611720f180d9d
Lalo Freak <lalofreak.dev@gmail.com>
2023-04-21 15:02:56 -0400

    thrash

--- src/components/Curriculum/Curriculum.jsx
+++ src/components/Curriculum/Curriculum.jsx
@@ -3,6 +3,7 @@ import s from './css/Curriculum.module.css'
 export const Curriculum = () => {
     return (
         <div className={s.contCV}>
+            <iframe title='cv' src="https://docs.google.com/document/d/e/2PACX-1vQdGTjkvYx86REziyJbLrbguZ6tqPPKtaOboizE24L5J7AlC5WalUhYIMBYSELKTzeUYTfKBCtXt9f8/pub?embedded=true"></iframe>
         </div>
     )
 }
