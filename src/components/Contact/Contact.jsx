9837f97198b35c4f87a2241af9f7226981af4d09
Lalo Freak <lalofreak.dev@gmail.com>
2023-04-26 20:37:24 -0400

    thrash

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,64 @@
+import React, { useState } from "react";
+import { OptionTitle } from "../Options/OptionTitle";
+import { useSelector } from "react-redux";
+import s from './css/Contact.module.css';
+import { URL_API } from "../../middlewares/misc/config";
 import { UnderConstruction } from "../Utils/UnderConstruction"
 
 export const Contact = () => {
+  const language = useSelector(state=>state.language)
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`${URL_API}/sendemail`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // show success message or handle error
+    })
+    .catch((error) => {
+      console.error(error);
+      // show error message or handle error
+    });
+  };
+  
   return (
-    <UnderConstruction/>
-  )
+    
+    <>
+    
+      <OptionTitle title={language==='EN'? 'contact' : 'contacto'} />
+      <form className={s.formCont} onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" onChange={handleInputChange}/>
+        </label><br />
+        <label>
+          Email:
+          <input type="email" name="email" onChange={handleInputChange} />
+        </label><br />
+        <label>
+          Message:
+          <textarea name="message" onChange={handleInputChange} />
+        </label><br/>
+        <button type="submit">Send</button>
+      </form>
+      <UnderConstruction />
+    </>
+  );
 }
