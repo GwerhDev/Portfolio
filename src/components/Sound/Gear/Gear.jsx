c04b3b1c251ef85e73ef6e35f1f33f6f7d584894
Lalo Freak <lalofreak.dev@gmail.com>
2023-04-28 00:26:27 -0400

    thrash

--- src/components/Sound/Gear/Gear.jsx
+++ src/components/Sound/Gear/Gear.jsx
@@ -1,6 +1,8 @@
-import { UnderConstruction } from '../../Utils/UnderConstruction'
 import s from './css/Gear.module.css'
-
+import qu16Img from '../../../images/qu16-img.png'
+import scarletImg from '../../../images/18i20.png'
+import sessionImg from '../../../images/akg-session-img.png'
+import titanImg from '../../../images/titan15d.png'
 
 export const Gear = (props) => {
     const {language} = props
@@ -9,8 +11,31 @@ export const Gear = (props) => {
             <div style={{width:'100%', textAlign:'center', paddingTop:'100px'}}>
                 <h1 style={{fontFamily:'Trajan', color:'white', fontSize:'2.3rem'}}>{language==='EN'? 'Gear' : 'Equipamiento'}</h1>
             </div>
-            <ul className={s.iconList} style={{marginLeft:'-40px'}}>
-             <UnderConstruction color={'white'}/>
+            <ul className={s.gearList} style={{marginLeft:'-40px'}}>
+                <li >
+                    <div className={s.imgCont}>
+                        <img className={s.gearImg} src={qu16Img} alt="" width='250px' />
+                        <p>Allen & Heath - Qu16</p>
+                    </div>
+                </li>
+                <li >
+                    <div className={s.imgCont}>
+                        <img className={s.gearImg} src={scarletImg} alt="" width='250px' />
+                        <p>Focusrite - Scarlett 18i20 1st gen</p>
+                    </div>
+                </li>
+                <li >
+                    <div className={s.imgCont}>
+                        <img className={s.gearImg} src={sessionImg} alt="" width='250px' />
+                        <p>AKG - Session 1 (drum kit)</p>
+                    </div>
+                </li>
+                <li >
+                    <div className={s.imgCont}>
+                        <img className={s.gearImg} src={titanImg} alt="" width='250px' />
+                        <p>WHAFEDALE - Titan 15D (Par activo)</p>
+                    </div>
+                </li>
             </ul>
         </div>
     )
