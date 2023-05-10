b1104e788c53bbf1daf6dd9d738581cfcc8a1869
Lalo Freak <lalofreak.dev@gmail.com>
2023-05-09 20:41:05 -0400

    landing

--- src/components/Landing/Nav.jsx
+++ src/components/Landing/Nav.jsx
@@ -10,7 +10,7 @@ export const Nav = () => {
                 <ul className={s.ulMenu} style={{color:"white"}}>
                     <li ><a href="http://projects.terminalkiller.site" rel="noreferrer"> <p style={{color:"white", marginTop:"0px"}} > Inicio </p></a></li>
                     <li style={{color:"white"}}><Link to="/" rel="noreferrer"><p style={{color:"white", marginTop:"0px"}} >Quiénes somos</p></Link></li>
-                    <li style={{color:"white"}}><a href="http://projects.terminalkiller.site/#/contact" rel="noreferrer"><p style={{color:"white", marginTop:"0px"}} >Contacto</p></a></li>
+                    {/*<li style={{color:"white"}}><a href="http://projects.terminalkiller.site/#/contact" rel="noreferrer"><p style={{color:"white", marginTop:"0px"}} >Contacto</p></a></li>*/}
                 </ul>
             </ul>
         </div>
