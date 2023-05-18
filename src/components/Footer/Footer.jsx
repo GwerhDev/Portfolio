b26c7e08df702f5c3bec9a2b7df5e17fe3079be9
Lalo Freak <lalofreak.dev@gmail.com>
2023-05-18 00:21:17 -0400

    styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ export const Footer = () => {
             color:'white', 
             marginTop:'0px'}
           }>
-            {currentYear()} &copy; Theme by {<a style={{color: 'white'}} href='https://github.com/LaloFreak/Portfolio' alt="repository" target='_blank' rel='noreferrer' >LaloFreak</a>}. All Cats Are Beautiful</h3>
+            {currentYear()} &copy; Theme by {<a style={{color: 'white'}} href='https://github.com/LaloFreak' alt="repository" target='_blank' rel='noreferrer' >LaloFreak</a>}. {<a style={{color: 'white'}} href='https://github.com/LaloFreak/Portfolio' alt="repository" target='_blank' rel='noreferrer' >All Cats Are Beautyful</a>}</h3>
     </div>
   )
 }
