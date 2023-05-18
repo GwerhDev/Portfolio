fcf75113edaa53d4eab9fd9b29ed343d375ee995
Lalo Freak <lalofreak.dev@gmail.com>
2023-05-18 00:04:43 -0400

    styles

--- src/components/Develop/Featured/Featured.jsx
+++ src/components/Develop/Featured/Featured.jsx
@@ -16,7 +16,9 @@ export const Featured = () => {
                 <div className={s.featuredWrap}>
                     <div className={s.titleItem}>
                         <h2>Artificial Intelligence app</h2>
-                        <img src={githubIcon} alt="github" />
+                        <a href="https://github.com/LaloFreak/ArtificialIntelligence-Client" target="_blank" rel="noreferrer">
+                            <img src={githubIcon} alt="github" />
+                        </a>
                     </div>
                     <a href="https://terminalkiller.site/#/ai-testing" target="_blank" rel="noreferrer">
                         <div className={s.wrapper}>
@@ -37,7 +39,9 @@ export const Featured = () => {
                         </a> 
                         <div className={s.titleItem}>
                             <h2>la Ruina tv</h2>
-                            <img src={githubIcon} alt="github" />
+                            <a href="https://github.com/LaloFreak/La-Ruina-TV-client" target="_blank" rel="noreferrer">
+                                <img src={githubIcon} alt="github" />
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -45,8 +49,9 @@ export const Featured = () => {
                     <div className={s.featuredWrap}>
                         <div className={s.titleItem}>
                             <h2>la Ruina tv</h2>
-                            <img src={githubIcon} alt="github" />
-                        </div>
+                            <a href="https://github.com/LaloFreak/La-Ruina-TV-client" target="_blank" rel="noreferrer">
+                                <img src={githubIcon} alt="github" />
+                            </a>                        </div>
                         <a href="https://tv.laruinarecords.cl" target="_blank" rel="noreferrer">
                             <div className={s.wrapper}>
                                 <div className={s.imageContainer}>
@@ -59,7 +64,9 @@ export const Featured = () => {
                 <div className={s.featuredWrap}>
                     <div className={s.titleItem}>
                         <h2>world of Gwerh</h2>
-                        <img src={githubIcon} alt="github" />
+                        <a href="https://github.com/LaloFreak/World-of-Gwerh" target="_blank" rel="noreferrer">
+                            <img src={githubIcon} alt="github" />
+                        </a>
                     </div>
                     <a href="http://worldofgwerh.terminalkiller.site" target="_blank" rel="noreferrer">
                         <div className={s.wrapper}>
