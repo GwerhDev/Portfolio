54f94fe0c60668ffb538581f94a2cb8e1c2ace65
Lalo Freak <lalofreak.dev@gmail.com>
2023-05-22 13:08:48 -0400

    buttons, links, styles

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -27,7 +27,7 @@ export const Menu = () => {
                 className={s.divH2}
                 onClick={()=>{return (
                     dispatch(setOption('bio')),
-                    document.querySelector('#profileLalo').style.transform='translateX(-30vw)',
+                    document.querySelector('#profileLalo').style.transform='translateX(-40vw)',
                     document.querySelector('#navCont').style.transform='translateX(0)'
                 )}}
             >
@@ -39,7 +39,7 @@ export const Menu = () => {
                 className={s.divH2}
                 onClick={()=>{return (
                     dispatch(setOption('cv')),
-                    document.querySelector('#profileLalo').style.transform='translateX(-30vw)',
+                    document.querySelector('#profileLalo').style.transform='translateX(-40vw)',
                     document.querySelector('#navCont').style.transform='translateX(0)'
                     )}}
                     >
