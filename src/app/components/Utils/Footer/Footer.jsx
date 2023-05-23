c411ed6a93ec9a075411373c66c48b069e6706a3
Lalo Freak <lalofreak.dev@gmail.com>
2023-05-23 12:23:52 -0400

    footer styles

--- src/app/components/Utils/Footer/Footer.jsx
+++ src/app/components/Utils/Footer/Footer.jsx
@@ -1,12 +1,21 @@
-import { currentYear } from '../../../../functions/FooterFunctions'
-import s from './css/Footer.module.css'
+import { currentYear } from '../../../../functions/FooterFunctions';
+import s from './css/Footer.module.css';
 
 export const Footer = () => {
   return (
     <div className={s.footerCont}>
-      <h3 
-        style={{ fontSize:'10px' }}>
-            {currentYear()} &copy; Theme by {<a style={{color: 'white'}} href='https://github.com/LaloFreak' alt="repository" target='_blank' rel='noreferrer' >LaloFreak</a>}. {<a style={{color: 'white'}} href='https://github.com/LaloFreak/Portfolio' alt="repository" target='_blank' rel='noreferrer' >All Cats Are Beautiful</a>}</h3>
+      <h3 style={{ fontSize: '10px' }}>
+        {currentYear()} &copy; Theme by LaloFreak.{' '}
+        <a
+          style={{ color: 'white', textDecoration: 'underline' }}
+          href='https://github.com/LaloFreak/Portfolio'
+          alt='repository'
+          target='_blank'
+          rel='noreferrer'
+        >
+          All Cats Are Beautiful
+        </a>
+      </h3>
     </div>
-  )
-}
+  );
+};
