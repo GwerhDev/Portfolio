8eb1b6ab8840a603c8aacfdebd5ab35de85b8c8a
Lalo Freak <lalofreak.dev@gmail.com>
2023-05-31 04:00:12 -0400

    slider

--- src/app/components/Utils/Slider/Slider.jsx
+++ src/app/components/Utils/Slider/Slider.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import s from './css/Slider.module.css'
 import { next, previous } from './js/Slider';
+import arrowIcon from '../../../../assets/images/png/showmore-icon.png'
 
 export const Slider = ({ children }) => {
   const sliderRef = useRef(null);
@@ -8,7 +9,9 @@ export const Slider = ({ children }) => {
 
   return (
     <div className={s.container}>
-      <button id={s.prev} className={s.buttons} onClick={()=>{return previous(k, setK)}}>{'<'}</button>
+      <button id={s.prev} className={s.buttons} onClick={()=>{return previous(k, setK)}}>
+        <img className={s.prevButton} width={20} src={arrowIcon} alt="" />
+      </button>
       <div className={s.slider} id={'slider'} ref={sliderRef}>
         <div className={s.sliderViewer} id='sliderViewwer'>
           <div className={s.sliderCont}>
@@ -16,7 +19,9 @@ export const Slider = ({ children }) => {
           </div>
         </div>
       </div>
-      <button id={s.next} className={s.buttons} onClick={()=>{return next(k, setK)}}>{'>'}</button>
+      <button id={s.next} className={s.buttons} onClick={()=>{return next(k, setK)}}>
+        <img className={s.nextButton} width={20} src={arrowIcon} alt="" /> 
+      </button>
       <div>
       </div>
     </div>
--- src/app/components/Utils/Slider/js/Slider.js
+++ src/app/components/Utils/Slider/js/Slider.js
@@ -1,30 +1,35 @@
 export const next = (k, setK) => {
   if(k<=1) {
     setK(2);
-    document.getElementById('first').style.opacity = `0`;
-    document.getElementById('first').style.scale = `0`;
+    document.getElementById('first').style.opacity = `0.4`;
+    document.getElementById('first').style.transformOrigin = `right`;    
+    document.getElementById('first').style.transform = `rotateY(-90deg)`;    
     document.getElementById('second').style.opacity = `1`;
-    document.getElementById('second').style.scale = `1`;
-    document.getElementById('third').style.opacity = `0`;
-    document.getElementById('third').style.scale = `0`;
+    document.getElementById('second').style.transformOrigin = `left`;
+    document.getElementById('second').style.transform = `rotateY(0deg)`; 
+    document.getElementById('third').style.opacity = `0.4`;
     document.getElementById('slider').style.transform = `translateX(-50vw)`;    
   } else if(k===2) {
     setK(3);
-    document.getElementById('first').style.opacity = `0`;
-    document.getElementById('first').style.scale = `0`;
-    document.getElementById('second').style.opacity = `0`;
-    document.getElementById('second').style.scale = `0`;
+    document.getElementById('first').style.opacity = `0.4`;
+    document.getElementById('second').style.opacity = `0.4`;
+    document.getElementById('second').style.transformOrigin = `right`;
+    document.getElementById('second').style.transform = `rotateY(-90deg)`;
     document.getElementById('third').style.opacity = `1`;
-    document.getElementById('third').style.scale = `1`;
+    document.getElementById('third').style.transformOrigin = `left`;
+    document.getElementById('third').style.transform = `rotateY(0deg)`; 
     document.getElementById('slider').style.transform = `translateX(-100vw)`;
   } else {
     setK(0);
     document.getElementById('first').style.opacity = `1`;
-    document.getElementById('first').style.scale = `1`;
-    document.getElementById('second').style.opacity = `0`;
-    document.getElementById('second').style.scale = `0`;
-    document.getElementById('third').style.opacity = `0`;
-    document.getElementById('third').style.scale = `0`;
+    document.getElementById('first').style.transformOrigin = `right`;
+    document.getElementById('first').style.transform = `rotateY(0deg)`;
+    document.getElementById('second').style.transformOrigin = `left`;
+    document.getElementById('second').style.transform = `rotateY(90deg)`;
+    document.getElementById('second').style.opacity = `0.4`;
+    document.getElementById('third').style.opacity = `0.4`;
+    document.getElementById('third').style.transformOrigin = `left`;
+    document.getElementById('third').style.transform = `rotateY(-90deg)`; 
     document.getElementById('slider').style.transform = `translateX(0)`;
   }
 };
@@ -33,29 +38,34 @@ export const previous = (k, setK) => {
   if(k===2) {
     setK(1);
     document.getElementById('first').style.opacity = `1`;
-    document.getElementById('first').style.scale = `1`;
-    document.getElementById('second').style.opacity = `0`;
-    document.getElementById('second').style.scale = `0`;
-    document.getElementById('third').style.opacity = `0`;
-    document.getElementById('third').style.scale = `0`;
+    document.getElementById('first').style.transformOrigin = `right`;
+    document.getElementById('first').style.transform = `rotateY(-0deg)`;    
+    document.getElementById('second').style.opacity = `0.4`;
+    document.getElementById('second').style.transformOrigin = `left`;
+    document.getElementById('second').style.transform = `rotateY(-90deg)`;    
+    document.getElementById('third').style.opacity = `0.4`;
     document.getElementById('slider').style.transform = `translateX(0vw)`;
   } else if(k===3) {
     setK(2);
-    document.getElementById('first').style.opacity = `0`;
-    document.getElementById('first').style.scale = `0`;
+    document.getElementById('first').style.opacity = `0.4`;
     document.getElementById('second').style.opacity = `1`;
-    document.getElementById('second').style.scale = `1`;
-    document.getElementById('third').style.opacity = `0`;
-    document.getElementById('third').style.scale = `0`;
+    document.getElementById('second').style.transformOrigin = `right`;
+    document.getElementById('second').style.transform = `rotateY(-0deg)`;    
+    document.getElementById('third').style.opacity = `0.4`;
+    document.getElementById('third').style.transformOrigin = `left`;
+    document.getElementById('third').style.transform = `rotateY(-90deg)`;    
     document.getElementById('slider').style.transform = `translateX(-50vw)`;
   } else {
     setK(3);
-    document.getElementById('first').style.opacity = `0`;
-    document.getElementById('first').style.scale = `0`;
-    document.getElementById('second').style.opacity = `0`;
-    document.getElementById('second').style.scale = `0`;
+    document.getElementById('first').style.opacity = `0.4`;
+    document.getElementById('first').style.transformOrigin = `right`;
+    document.getElementById('first').style.transform = `rotateY(90deg)`;  
+    document.getElementById('second').style.transformOrigin = `right`;
+    document.getElementById('second').style.transform = `rotateY(90deg)`;  
+    document.getElementById('second').style.opacity = `0.4`;
     document.getElementById('third').style.opacity = `1`;
-    document.getElementById('third').style.scale = `1`;
+    document.getElementById('third').style.transformOrigin = `right`;
+    document.getElementById('third').style.transform = `rotateY(0deg)`;
     document.getElementById('slider').style.transform = `translateX(-100vw)`;
   }
 };
