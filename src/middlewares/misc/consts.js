74fd31e283ebd5207780cb8a22fd29a0ddbe50ad
Lalo Freak <lalofreak.dev@gmail.com>
2023-06-21 18:39:59 -0400

    basic model and functions

--- src/middlewares/misc/consts.js
+++ src/middlewares/misc/consts.js
@@ -10,14 +10,14 @@ export const SET_INFO = 'SET_INFO'
 
 
 export const GET_DESCRIPTION_DEV={
-    en:'I am a Fullstack Web Developer oriented to Multimedia, Scalable Apps, Machine Learning and Artificial Intelligence.',
-    es:'Soy un Desarrollador Web Fullstack, orientado a Multimedia, Aplicaciones Escalables, Machine Learning e Inteligencia Artificial.'
+    en:'Fullstack Web Developer oriented to Multimedia, Scalable Apps, Machine Learning and Artificial Intelligence.',
+    es:'Desarrollador Web Fullstack, orientado a Multimedia, Aplicaciones Escalables, Machine Learning e Inteligencia Artificial.'
 }
 export const GET_DESCRIPTION_SOUND={
-    en:"I am a self-taught Soundman, founder of the Audio Recording and Post-production Studio 'La Ruina Records'.",
-    es:"Soy un Sonidista autodidacta, fundador del Estudio de Grabación y Post-producción de Audio 'La Ruina Records'."
+    en:"Self-taught Soundman, founder of the Audio Recording and Post-production Studio 'La Ruina Records'.",
+    es:"Sonidista autodidacta, fundador del Estudio de Grabación y Post-producción de Audio 'La Ruina Records'."
 }
 export const GET_DESCRIPTION_DESIGN={
-    en:"I am a designer oriented to 3D modeling and operator of CNC Routers, CNC Lasers and 3D Printers.",
-    es:"Soy un Diseñador orientado al modelado 3D y operador de maquinaria CNC Router, CNC Láser e Impresoras 3D."
+    en:"Designer oriented to 3D modeling and operator of CNC Routers, CNC Lasers and 3D Printers.",
+    es:"Diseñador orientado al modelado 3D y operador de maquinaria CNC Router, CNC Láser e Impresoras 3D."
 }
