bc3543c2805de3e83f19e65b203a2ac4a2a5e172
Lalo Freak <lalofreak.dev@gmail.com>
2023-07-06 10:35:48 -0400

    styles

--- src/app/components/Profile/Profile.jsx
+++ src/app/components/Profile/Profile.jsx
@@ -1,11 +1,7 @@
 import s from "./css/Profile.module.css";
-import { useDispatch } from "react-redux";
-import { resetOption } from "../../../middlewares/redux/actions";
 import picLaloFreak from "../../../assets/images/png/LaloFreak.png";
-import { profilePicClick } from "../../../functions/ProfileFunctions";
 
 export const Profile = () => {
-    const dispatch = useDispatch()
     return (
     <>
         <img 
@@ -13,7 +9,6 @@ export const Profile = () => {
             src={picLaloFreak} 
             alt="LaloFreak" 
             width="150px" 
-            onClick={()=> profilePicClick(dispatch, resetOption)}
         />
         <h2 className={s.name}>Gerardo guarda</h2>
         <h3 className={s.alias}>gwerh</h3>
