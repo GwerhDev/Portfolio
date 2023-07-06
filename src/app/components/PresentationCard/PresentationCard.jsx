799f5aab12612f16ddeb410aaf62c836c8a0d660
Lalo Freak <lalofreak.dev@gmail.com>
2023-07-06 10:13:18 -0400

    styles

--- src/app/components/PresentationCard/PresentationCard.jsx
+++ src/app/components/PresentationCard/PresentationCard.jsx
@@ -6,50 +6,50 @@ export const PresentationCard = (props) => {
     const { img, language, description, background } = props
     return (
       <div className={s.container}>
-      <motion.div 
-        initial={{opacity:0}}
-        transition={{duration: 1}}
-        animate={{opacity:1}}
-      >
-      <div className={s.viewerCont} id='presentationCardSection' style={{backgroundImage: `url(${background})`}}>
-        <div className={s.imgCont}>
-          <img className={s.portfolioImg} src={img} alt="" loading="lazy" />
-        </div>
-      <ul className={s.viewerUl}>
-        <h1 className={s.title}>Gerardo guarda
-        <br/>
-          <p>gwerh</p>
-          <div className={s.introCont}>
-            <b className={s.parr}>
-              {
-                language==='EN'?
-                description.en
-                :
-                description.es
-              }
-            </b>
-            <br />
-            <button className={s.resumeButton}>
-              <Link to='/home/cv' className={s.resumeLink}>
-                  {
-                    language==='EN'?
-                    'check resume' : 'ver cv'
-                  }
-              </Link>
-            </button>
-            <button className={s.resumeButton}>
-              <Link to='/contact' className={s.resumeLink}>
-                  {
-                    language==='EN'?
-                    'contact' : 'contacto'
-                  }
-              </Link>
-            </button>
+        <motion.div 
+          initial={{opacity:0}}
+          transition={{duration: 1}}
+          animate={{opacity:1}}
+        >
+          <div className={s.viewerCont} id='presentationCardSection' style={{backgroundImage: `url(${background})`}}>
+            <div className={s.imgCont}>
+              <img className={s.portfolioImg} src={img} alt="" loading="lazy" />
+            </div>
+            <ul className={s.viewerUl}>
+              <h1 className={s.title}>Gerardo guarda
+              <br/>
+                <p>gwerh</p>
+                <div className={s.introCont}>
+                  <b className={s.parr}>
+                    {
+                      language==='EN'?
+                      description.en
+                      :
+                      description.es
+                    }
+                  </b>
+                  <br />
+                  <button className={s.resumeButton}>
+                    <Link to='/home/cv' className={s.resumeLink}>
+                        {
+                          language==='EN'?
+                          'check resume' : 'ver cv'
+                        }
+                    </Link>
+                  </button>
+                  <button className={s.resumeButton}>
+                    <Link to='/contact' className={s.resumeLink}>
+                        {
+                          language==='EN'?
+                          'contact' : 'contacto'
+                        }
+                    </Link>
+                  </button>
+                </div>
+              </h1>
+            </ul>
           </div>
-        </h1>
-      </ul>
-      </div>
-      </motion.div>
+        </motion.div>
       </div>
 
     )
