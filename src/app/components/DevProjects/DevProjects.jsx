0ec7f053f2537010c75b036f1430cd22b7f09b97
Lalo Freak <lalofreak.dev@gmail.com>
2023-07-14 00:11:55 -0400

    styles

--- src/app/components/DevProjects/DevProjects.jsx
+++ src/app/components/DevProjects/DevProjects.jsx
@@ -19,55 +19,23 @@ export const DevProjects = (props) => {
 
   return (
     <div className={s.devCont}>
-      <NavFilter language={language}/>
-      <ul className={s.devUlCont} id='devListWebSection'>
-        <div className={s.cathegoryDescription}>
-          <h1 className={s.title} id="title">Web</h1>
-          <p>{language==='EN'?'Web Applications':'Aplicaciones Web'}</p>
-        </div>
-        {
-          API? API.at(0)?.web?.map((e,index)=>{
-          return(
-            <li 
-            onClick={()=>{
-              return (
-                dispatch(setInfo({
-                  img: e.idImg? RenderDriveImg(e.idImg): loadingImg,
-                  title: e.name,
-                  type: 'web',
-                  description: language==='EN'? `Description: ${e.description.en}` : `Descripción: ${e.description.es}`,
-                  technologies: language==='EN'? `Technologies: ${e.technologies.en}` : `Tecnologías: ${e.technologies.es}`,
-                  role: language==='EN'? `Role: ${e.role.en}` : `Rol: ${e.role.es}`,
-                  url: e.href,
-                  github: e.repository
-                })), 
-                openInfoCanvas()
-              )
-            }}
-              key={`ulWeb${index}`} className={s.devLiCont} style={{backgroundImage:`url(${e.idImg? RenderDriveImg(e.idImg): loadingImg})`, backgroundSize:`${e.idImg? 'cover': '30px'} `}}>
-              <div className={s.backImgCanvas}>
-                <h1>{e.name}</h1>
-              </div>
-            </li>
-          )
-        }) : <Loader language={language}/>
-      }
-      </ul>
-      <ul className={s.devUlCont} id='devListDesktopSection'>
-        <div className={s.cathegoryDescription}>
-          <h1 className={s.title} id="title">{language==='EN'?'Desktop':'Escritorio'}</h1>
-          <p>{language==='EN'?'Desktop Applications':'Aplicaciones de Escritorio'}</p>
-        </div>
-        {
-          API? API.at(1)?.desktop?.map((e,index)=>{
-          return(
-            <li 
+      <section className={s.projectSection}>
+        <NavFilter language={language}/>
+        <ul className={s.devUlCont} id='devListWebSection'>
+          <div className={s.cathegoryDescription}>
+            <h1 className={s.title} id="title">Web</h1>
+            <p>{language==='EN'?'Web Applications':'Aplicaciones Web'}</p>
+          </div>
+          {
+            API? API.at(0)?.web?.map((e,index)=>{
+            return(
+              <li 
               onClick={()=>{
                 return (
                   dispatch(setInfo({
                     img: e.idImg? RenderDriveImg(e.idImg): loadingImg,
                     title: e.name,
-                    type: 'desktop',
+                    type: 'web',
                     description: language==='EN'? `Description: ${e.description.en}` : `Descripción: ${e.description.es}`,
                     technologies: language==='EN'? `Technologies: ${e.technologies.en}` : `Tecnologías: ${e.technologies.es}`,
                     role: language==='EN'? `Role: ${e.role.en}` : `Rol: ${e.role.es}`,
@@ -77,81 +45,115 @@ export const DevProjects = (props) => {
                   openInfoCanvas()
                 )
               }}
-              key={`ulDesk${index}`} className={s.devLiCont} style={{backgroundImage:`url(${e.idImg? RenderDriveImg(e.idImg): loadingImg})`, backgroundSize:`${e.idImg? 'cover': '30px'} `}}>
-              <div className={s.backImgCanvas}>
-                <h1>{e.name}</h1>
-              </div>
-            </li>
-          )
-        }) : <Loader language={language}/>
-      }
-      </ul>
-      <ul className={s.devUlCont} id='devListMobileSection'>
-        <div className={s.cathegoryDescription}>
-          <h1 className={s.title} id="title">{language==='EN'?'Mobile':'Movil'}</h1>
-          <p>{language==='EN'?'Mobile Applications':'Aplicaciones de Móvil'}</p>
-        </div>
-        {
-          API? API.at(2)?.mobile?.map((e,index)=>{
-          return(
-            <li 
-              onClick={()=>{
-                return (
-                  dispatch(setInfo({
-                    img: e.idImg? RenderDriveImg(e.idImg): loadingImg,
-                    title: e.name,
-                    type: 'mobile',
-                    description: language==='EN'? `Description: ${e.description.en}` : `Descripción: ${e.description.es}`,
-                    technologies: language==='EN'? `Technologies: ${e.technologies.en}` : `Tecnologías: ${e.technologies.es}`,
-                    role: language==='EN'? `Role: ${e.role.en}` : `Rol: ${e.role.es}`,
-                    url: e.href,
-                    github: e.repository
-                  })), 
-                  openInfoCanvas()
-                )
-              }}
-              key={`ulMobile${index}`} className={s.devLiCont} style={{backgroundImage:`url(${e.idImg? RenderDriveImg(e.idImg): loadingImg})`, backgroundSize:`${e.idImg? 'cover': '30px'} `}}>
-              <div className={s.backImgCanvas}>
-                <h1>{e.name}</h1>
-              </div>
-            </li>
-          )
-        }) : <Loader language={language}/>
-      }
-      </ul>
-      <ul className={s.devUlCont} id='devListSoftwareSection'>
-        <div className={s.cathegoryDescription}>
-          <h1 className={s.title} id="title">Software</h1>
-          <p>{language==='EN'?'Software developing':'Desarrollo de Software'}</p>
-        </div>
-        {
-          API? API.at(3)?.software?.map((e,index)=>{
-          return(
-            <li 
-              onClick={()=>{
-                return (
-                  dispatch(setInfo({
-                    img: e.idImg? RenderDriveImg(e.idImg): loadingImg,
-                    title: e.name,
-                    type: 'software',
-                    description: language==='EN'? `Description: ${e.description.en}` : `Descripción: ${e.description.es}`,
-                    technologies: language==='EN'? `Technologies: ${e.technologies.en}` : `Tecnologías: ${e.technologies.es}`,
-                    role: language==='EN'? `Role: ${e.role.en}` : `Rol: ${e.role.es}`,
-                    url: e.href,
-                    github: e.repository
-                  })), 
-                  openInfoCanvas()
-                )
-              }}
-              key={`ulSoft${index}`} className={s.devLiCont} style={{backgroundImage:`url(${e.idImg? RenderDriveImg(e.idImg): loadingImg})`, backgroundSize:`${e.idImg? 'cover': '30px'} `}}>
-              <div className={s.backImgCanvas}>
-                <h1>{e.name}</h1>
-              </div>
-            </li>
-          )
-        }) : <Loader language={ language }/>
-      }
-      </ul>
+                key={`ulWeb${index}`} className={s.devLiCont} style={{backgroundImage:`url(${e.idImg? RenderDriveImg(e.idImg): loadingImg})`, backgroundSize:`${e.idImg? 'cover': '30px'} `}}>
+                <div className={s.backImgCanvas}>
+                  <h1>{e.name}</h1>
+                </div>
+              </li>
+            )
+          }) : <Loader language={language}/>
+        }
+        </ul>
+        <ul className={s.devUlCont} id='devListDesktopSection'>
+          <div className={s.cathegoryDescription}>
+            <h1 className={s.title} id="title">{language==='EN'?'Desktop':'Escritorio'}</h1>
+            <p>{language==='EN'?'Desktop Applications':'Aplicaciones de Escritorio'}</p>
+          </div>
+          {
+            API? API.at(1)?.desktop?.map((e,index)=>{
+            return(
+              <li 
+                onClick={()=>{
+                  return (
+                    dispatch(setInfo({
+                      img: e.idImg? RenderDriveImg(e.idImg): loadingImg,
+                      title: e.name,
+                      type: 'desktop',
+                      description: language==='EN'? `Description: ${e.description.en}` : `Descripción: ${e.description.es}`,
+                      technologies: language==='EN'? `Technologies: ${e.technologies.en}` : `Tecnologías: ${e.technologies.es}`,
+                      role: language==='EN'? `Role: ${e.role.en}` : `Rol: ${e.role.es}`,
+                      url: e.href,
+                      github: e.repository
+                    })), 
+                    openInfoCanvas()
+                  )
+                }}
+                key={`ulDesk${index}`} className={s.devLiCont} style={{backgroundImage:`url(${e.idImg? RenderDriveImg(e.idImg): loadingImg})`, backgroundSize:`${e.idImg? 'cover': '30px'} `}}>
+                <div className={s.backImgCanvas}>
+                  <h1>{e.name}</h1>
+                </div>
+              </li>
+            )
+          }) : <Loader language={language}/>
+        }
+        </ul>
+        <ul className={s.devUlCont} id='devListMobileSection'>
+          <div className={s.cathegoryDescription}>
+            <h1 className={s.title} id="title">{language==='EN'?'Mobile':'Movil'}</h1>
+            <p>{language==='EN'?'Mobile Applications':'Aplicaciones de Móvil'}</p>
+          </div>
+          {
+            API? API.at(2)?.mobile?.map((e,index)=>{
+            return(
+              <li 
+                onClick={()=>{
+                  return (
+                    dispatch(setInfo({
+                      img: e.idImg? RenderDriveImg(e.idImg): loadingImg,
+                      title: e.name,
+                      type: 'mobile',
+                      description: language==='EN'? `Description: ${e.description.en}` : `Descripción: ${e.description.es}`,
+                      technologies: language==='EN'? `Technologies: ${e.technologies.en}` : `Tecnologías: ${e.technologies.es}`,
+                      role: language==='EN'? `Role: ${e.role.en}` : `Rol: ${e.role.es}`,
+                      url: e.href,
+                      github: e.repository
+                    })), 
+                    openInfoCanvas()
+                  )
+                }}
+                key={`ulMobile${index}`} className={s.devLiCont} style={{backgroundImage:`url(${e.idImg? RenderDriveImg(e.idImg): loadingImg})`, backgroundSize:`${e.idImg? 'cover': '30px'} `}}>
+                <div className={s.backImgCanvas}>
+                  <h1>{e.name}</h1>
+                </div>
+              </li>
+            )
+          }) : <Loader language={language}/>
+        }
+        </ul>
+        <ul className={s.devUlCont} id='devListSoftwareSection'>
+          <div className={s.cathegoryDescription}>
+            <h1 className={s.title} id="title">Software</h1>
+            <p>{language==='EN'?'Software developing':'Desarrollo de Software'}</p>
+          </div>
+          {
+            API? API.at(3)?.software?.map((e,index)=>{
+            return(
+              <li 
+                onClick={()=>{
+                  return (
+                    dispatch(setInfo({
+                      img: e.idImg? RenderDriveImg(e.idImg): loadingImg,
+                      title: e.name,
+                      type: 'software',
+                      description: language==='EN'? `Description: ${e.description.en}` : `Descripción: ${e.description.es}`,
+                      technologies: language==='EN'? `Technologies: ${e.technologies.en}` : `Tecnologías: ${e.technologies.es}`,
+                      role: language==='EN'? `Role: ${e.role.en}` : `Rol: ${e.role.es}`,
+                      url: e.href,
+                      github: e.repository
+                    })), 
+                    openInfoCanvas()
+                  )
+                }}
+                key={`ulSoft${index}`} className={s.devLiCont} style={{backgroundImage:`url(${e.idImg? RenderDriveImg(e.idImg): loadingImg})`, backgroundSize:`${e.idImg? 'cover': '30px'} `}}>
+                <div className={s.backImgCanvas}>
+                  <h1>{e.name}</h1>
+                </div>
+              </li>
+            )
+          }) : <Loader language={ language }/>
+        }
+        </ul>
+      </section>
     </div>
   )
 }
--- src/functions/ScrollToSection.js
+++ src/functions/ScrollToSection.js
@@ -1,4 +1,4 @@
 export const ScrollToSection = (id, behavior) => {
   const element = document.getElementById(id);
-  element.scrollIntoView({ behavior: behavior?? 'smooth' });
+  element?.scrollIntoView({ behavior: behavior?? 'smooth' });
 };
