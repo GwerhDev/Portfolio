4e5becca43c086e913c5eef18d082697089c65de
Lalo Freak <lalofreak.dev@gmail.com>
2023-07-23 18:44:12 -0400

    integrating 'DevDailyJoke'

--- src/app/components/Utils/DevDailyJoke/DevDailyJoke.jsx
+++ src/app/components/Utils/DevDailyJoke/DevDailyJoke.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import s from './css/DevDailyJoke.module.css';
+import { Loader } from '../Loader/Loader';
+import openWindow from '../../../../assets/images/png/openwindow-icon.png'
 
 export const DevDailyJoke = (props) => {
   const {language, devDailyJoke} = props;
@@ -7,9 +9,12 @@ export const DevDailyJoke = (props) => {
   return (
     <div className={s.jokeContainer}>
       <h3>
-        {language==='EN'? '"DEVJOKE" OF THE DAY': '"DEVJOKE" DEL DÍA'}
+        <a className={s.jokeTitle} href='https://github.com/GwerhDev/DevDailyJoke-API' target='_blank' rel='noreferrer'>
+          {language==='EN'? '"DEVJOKE" OF THE DAY': '"DEVJOKE" DEL DÍA'}
+          <img src={openWindow} alt="DevDailyJoke" width="18px"/>
+        </a>
       </h3>
-      <span>"{devDailyJoke}"</span>
+      { devDailyJoke? <span>{ `"${devDailyJoke}"` }</span> : <div><Loader/></div> }
     </div>
   )
 }
