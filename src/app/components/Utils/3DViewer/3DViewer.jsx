9dbe9650006ffe50d8fe5011a346bbaf16cd3e44
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-06 11:20:31 -0300

    adding 3d.viwer

--- src/app/components/Utils/3DViewer/3DViewer.jsx
+++ src/app/components/Utils/3DViewer/3DViewer.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
 import s from './3DViewer.module.css';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
@@ -8,47 +7,41 @@ export function Viewer(props) {
   const { file } = props;
   const canvasRef = useRef(null);
   const [progressBar, setProgressBar] = useState(0);
+  const [zoom, setZoom] = useState(5);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     canvas.width = 300;
     canvas.height = 300;
-
     const scene = new THREE.Scene();
-
+    scene.background = new THREE.Color(0x919191);
     const camera = new THREE.PerspectiveCamera(105, 1, 2, 1000);
     const renderer = new THREE.WebGLRenderer({ canvas });
     renderer.setSize(canvas.width, canvas.height);
-
     const loader = new OBJLoader();
     loader.load(
       file,
-      (objeto) => {
-        scene.add(objeto);
+      (object) => {
+        scene.add(object);
       },
-      (progreso) => {
-        setProgressBar(parseInt((progreso.loaded / progreso.total) * 100) + '%');
+      (progress) => {
+        setProgressBar(parseInt((progress.loaded / progress.total) * 100) + '%');
       },
       (error) => {
         console.error('Error al cargar el archivo .obj', error);
       }
     );
-
-    camera.position.z = 5;
-
+    camera.position.z = zoom;
     const light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(0, 1, 1);
     scene.add(light);
-
     const ambientLight = new THREE.AmbientLight(0xffffff, .2);
     scene.add(ambientLight);
-
     let isDragging = false;
     let previousMousePosition = {
       x: 0,
       y: 0
     };
-
     canvas.addEventListener('mousedown', (event) => {
       isDragging = true;
       previousMousePosition = {
@@ -56,33 +49,35 @@ export function Viewer(props) {
         y: event.clientY
       };
     });
-
     canvas.addEventListener('mousemove', (event) => {
       if (isDragging) {
         const deltaX = event.clientX - previousMousePosition.x;
         const deltaY = event.clientY - previousMousePosition.y;
-
         scene.rotation.y += deltaX * 0.01;
         scene.rotation.x += deltaY * 0.01;
-
         previousMousePosition = {
           x: event.clientX,
           y: event.clientY
         };
       }
     });
-
     canvas.addEventListener('mouseup', () => {
       isDragging = false;
     });
-
+    canvas.addEventListener('wheel', (event) => {
+      if (event.deltaY < 0) {
+        setZoom((prevZoom) => prevZoom - 0.1);
+      } else {
+        setZoom((prevZoom) => prevZoom + 0.1);
+      }
+    });
     const animate = () => {
       requestAnimationFrame(animate);
+      camera.position.z = zoom;
       renderer.render(scene, camera);
     };
     animate();
-  }, [file]);
-
+  }, [file, zoom]);
   return (
     <div className={s.container}>
       <div className={s.container}>
