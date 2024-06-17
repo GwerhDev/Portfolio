bc90d9fd0e0dd7437bbd9ce1dd420b31f63596a2
GwerhDev <gwerh.dev@gmail.com>
2024-06-17 14:45:49 -0400

    styling

--- src/app/pages/Options/OptionsCanvas.jsx
+++ src/app/pages/Options/OptionsCanvas.jsx
@@ -1,12 +1,12 @@
 import s from './css/Options.module.css';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setOption } from '../../../middlewares/redux/actions';
 import { Title } from '../../components/Utils/Title/Title';
 import { Intro } from './Intro';
 import { Bio } from './Bio';
 import { Cv } from './Cv';
-import { Link } from 'react-router-dom';
 import arrowIcon from '../../../assets/images/png/arrow-icon.png';
 
 export const OptionCanvas = () => {
@@ -35,60 +35,14 @@ export const OptionCanvas = () => {
               animate={{ opacity: 1, x: 0 }}
             >
 
-              {option === 'intro' ?
-                <><Intro />
-                  <div className={s.btnsCont}>
-                    <img src='true' alt='' />
-                    <div style={{ display: 'flex' }}>
-                      <h3 style={{ fontFamily: 'Trajan', fontSize: '18px', marginRight: '10px' }}>bio</h3>
-                      <Link to='/home/bio'>
-                        <img
-                          className={s.menuback}
-                          onClick={() => { return dispatch(setOption('bio')) }}
-                          src={arrowIcon}
-                          alt='next'
-                          width='30px'
-                        />
-                      </Link>
-                    </div>
-                  </div></>
-                : null}
-              {option === 'bio' ?
-                <><Bio />
-
-                  <div className={s.btnsCont}>
-                    <div style={{ display: 'flex' }}>
-                      <Link to='/home/intro'>
-                        <img
-                          className={s.menuback}
-                          onClick={() => { return dispatch(setOption('intro')) }}
-                          src={arrowIcon}
-                          alt='next'
-                          width='30px'
-                          style={{ rotate: '180deg' }}
-                        />
-                      </Link>
-                      <h3 style={{ fontFamily: 'Trajan', fontSize: '18px', marginLeft: '10px' }}>{language === 'EN' ? 'intro' : 'intro'}</h3>
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                      <h3 style={{ fontFamily: 'Trajan', fontSize: '18px', marginRight: '10px' }}>cv</h3>
-                      <Link to='/home/resume'>
-                        <img
-                          className={s.menuback}
-                          onClick={() => { return dispatch(setOption('resume')) }}
-                          src={arrowIcon}
-                          alt='next'
-                          width='30px'
-                        />
-                      </Link>
-                    </div>
-                  </div></>
-                : null}
-              {option === 'resume' ?
-                <><Cv />
-                  <div className={s.buttonsCont}>
+              {
+                option === 'intro'
+                  ?
+                  <><Intro />
                     <div className={s.btnsCont}>
+                      <img src='true' alt='' />
                       <div style={{ display: 'flex' }}>
+                        <h3 style={{ fontFamily: 'Trajan', fontSize: '18px', marginRight: '10px' }}>bio</h3>
                         <Link to='/home/bio'>
                           <img
                             className={s.menuback}
@@ -96,15 +50,73 @@ export const OptionCanvas = () => {
                             src={arrowIcon}
                             alt='next'
                             width='30px'
+                          />
+                        </Link>
+                      </div>
+                    </div></>
+                  :
+                  null
+              }
+              {
+                option === 'bio'
+                  ?
+                  <><Bio />
+
+                    <div className={s.btnsCont}>
+                      <div style={{ display: 'flex' }}>
+                        <Link to='/home/intro'>
+                          <img
+                            className={s.menuback}
+                            onClick={() => { return dispatch(setOption('intro')) }}
+                            src={arrowIcon}
+                            alt='next'
+                            width='30px'
                             style={{ rotate: '180deg' }}
                           />
                         </Link>
-                        <h3 style={{ fontFamily: 'Trajan', fontSize: '18px', marginLeft: '10px' }}>bio</h3>
+                        <h3 style={{ fontFamily: 'Trajan', fontSize: '18px', marginLeft: '10px' }}>{language === 'EN' ? 'intro' : 'intro'}</h3>
+                      </div>
+                      <div style={{ display: 'flex' }}>
+                        <h3 style={{ fontFamily: 'Trajan', fontSize: '18px', marginRight: '10px' }}>cv</h3>
+                        <Link to='/home/resume'>
+                          <img
+                            className={s.menuback}
+                            onClick={() => { return dispatch(setOption('resume')) }}
+                            src={arrowIcon}
+                            alt='next'
+                            width='30px'
+                          />
+                        </Link>
+                      </div>
+                    </div></>
+                  :
+                  null
+              }
+              {
+                option === 'resume'
+                  ?
+                  <><Cv />
+                    <div className={s.buttonsCont}>
+                      <div className={s.btnsCont}>
+                        <div style={{ display: 'flex' }}>
+                          <Link to='/home/bio'>
+                            <img
+                              className={s.menuback}
+                              onClick={() => { return dispatch(setOption('bio')) }}
+                              src={arrowIcon}
+                              alt='next'
+                              width='30px'
+                              style={{ rotate: '180deg' }}
+                            />
+                          </Link>
+                          <h3 style={{ fontFamily: 'Trajan', fontSize: '18px', marginLeft: '10px' }}>bio</h3>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </>
-                : null}
+                  </>
+                  :
+                  null
+              }
             </motion.div>
           </div>
         </section>
