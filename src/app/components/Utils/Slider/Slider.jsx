835865a7a2be4758e74ac79bc3a2695be7b046ce
Gwerh <gwerh.dev@gmail.com>
2024-06-25 15:07:59 -0400

    slider component now can receive any number of cards

--- src/app/components/Utils/Slider/Slider.jsx
+++ src/app/components/Utils/Slider/Slider.jsx
@@ -1,25 +1,29 @@
 import s from './css/Slider.module.css';
-import React, { useRef, useState } from 'react';
-import { next, previous } from './js/Slider';
+import React, { useEffect, useRef, useState } from 'react';
+import { initialState, next, previous } from './js/Slider';
 import arrowIcon from '../../../../assets/images/png/showmore-icon.png';
 
 export const Slider = (props) => {
   const { gallery } = props;
 
   const sliderRef = useRef(null);
-  const [k, setK] = useState(1);
+  const [k, setK] = useState(0);
 
   function handlePrevious() {
-    return previous(k, setK);
+    return previous(k, setK, gallery?.length);
   }
 
   function handleNext() {
-    return next(k, setK);
+    return next(k, setK, gallery?.length);
   }
+
+  useEffect(() => {
+    initialState(gallery?.length);
+  }, [gallery?.length]);
 
   return (
     <div className={s.container}>
-      <button id={s.prev} disabled={k <= 1} className={s.buttons} onClick={handlePrevious}>
+      <button id={s.prev} disabled={k <= 0} className={s.buttons} onClick={handlePrevious}>
         <img className={s.prevButton} width={20} src={arrowIcon} alt="" />
       </button>
       <div className={s.slider} id='slider' ref={sliderRef}>
@@ -33,7 +37,7 @@ export const Slider = (props) => {
           }
         </div>
       </div>
-      <button id={s.next} disabled={gallery?.length <= 2} className={s.buttons} onClick={handleNext}>
+      <button id={s.next} disabled={gallery?.length <= 1} className={s.buttons} onClick={handleNext}>
         <img className={s.nextButton} width={20} src={arrowIcon} alt="" />
       </button>
     </div>
--- src/app/components/Utils/Slider/js/Slider.js
+++ src/app/components/Utils/Slider/js/Slider.js
@@ -1,71 +1,91 @@
-export const next = (k, setK) => {
-  if (k === 1) {
-    setK(2);
-    document.getElementById(`card0`).style.opacity = `0.4`;
-    document.getElementById(`card0`).style.transformOrigin = `right`;
-    document.getElementById(`card0`).style.transform = `rotateY(-90deg)`;
-    document.getElementById(`card1`).style.opacity = `1`;
-    document.getElementById(`card1`).style.transformOrigin = `left`;
-    document.getElementById(`card1`).style.transform = `rotateY(0deg)`;
-    document.getElementById(`card2`).style.opacity = `0.4`;
-    document.getElementById(`slider`).style.transform = `translateX(-100%)`;
-  } else if (k === 2) {
-    setK(k + 1);
-    document.getElementById(`card0`).style.opacity = `0.4`;
-    document.getElementById(`card1`).style.opacity = `0.4`;
-    document.getElementById(`card1`).style.transformOrigin = `right`;
-    document.getElementById(`card1`).style.transform = `rotateY(-90deg)`;
-    document.getElementById(`card2`).style.opacity = `1`;
-    document.getElementById(`card2`).style.transformOrigin = `left`;
-    document.getElementById(`card2`).style.transform = `rotateY(0deg)`;
-    document.getElementById(`slider`).style.transform = `translateX(-200%)`;
-  } else {
+export const initialState = (length) => {
+  for (let i = 0; i < length; i++) {
+    document.getElementById(`card${i}`).style.transitionDuration = '.7s';
+    document.getElementById(`card${i}`).style.transform = 'rotateY(0deg)';
+    if (i > 0) {
+      document.getElementById(`card${i}`).style.opacity = '0.4';
+      document.getElementById(`card${i}`).style.transform = 'rotateY(90deg)';
+      document.getElementById(`card${i}`).style.transformOrigin = 'right';
+    }
+  }
+
+  return;
+}
+
+export const next = (k, setK, length) => {
+  if (k === 0) {
     setK(1);
+    document.getElementById(`card${k}`).style.opacity = `0.4`;
+    document.getElementById(`card${k}`).style.transformOrigin = `right`;
+    document.getElementById(`card${k}`).style.transform = `rotateY(-90deg)`;
+    document.getElementById(`card${k + 1}`).style.opacity = `1`;
+    document.getElementById(`card${k + 1}`).style.transformOrigin = `left`;
+    document.getElementById(`card${k + 1}`).style.transform = `rotateY(0deg)`;
+    document.getElementById(`card${k + 2}`).style.opacity = `0.4`;
+    document.getElementById(`slider`).style.transform = `translateX(-100%)`;
+  } else if (k < length - 1 && k > 0) {
+    setK(k + 1);
+    document.getElementById(`card${k - 1}`).style.opacity = `0.4`;
+    document.getElementById(`card${k}`).style.opacity = `0.4`;
+    document.getElementById(`card${k}`).style.transformOrigin = `right`;
+    document.getElementById(`card${k}`).style.transform = `rotateY(-90deg)`;
+    document.getElementById(`card${k + 1}`).style.opacity = `1`;
+    document.getElementById(`card${k + 1}`).style.transformOrigin = `left`;
+    document.getElementById(`card${k + 1}`).style.transform = `rotateY(0deg)`;
+    document.getElementById(`slider`).style.transform = `translateX(-${100 * (k + 1)}%)`;
+  } else {
+    setK(0);
     document.getElementById(`card0`).style.opacity = `1`;
     document.getElementById(`card0`).style.transformOrigin = `right`;
     document.getElementById(`card0`).style.transform = `rotateY(0deg)`;
-    document.getElementById(`card1`).style.transformOrigin = `left`;
-    document.getElementById(`card1`).style.transform = `rotateY(90deg)`;
-    document.getElementById(`card1`).style.opacity = `0.4`;
-    document.getElementById(`card2`).style.opacity = `0.4`;
-    document.getElementById(`card2`).style.transformOrigin = `left`;
-    document.getElementById(`card2`).style.transform = `rotateY(-90deg)`;
+
+    for (let i = 1; i < length; i++) {
+      document.getElementById(`card${i}`).style.opacity = `0.4`;
+      document.getElementById(`card${i}`).style.transformOrigin = `left`;
+      document.getElementById(`card${i}`).style.transform = `rotateY(90deg)`;
+    }
+
     document.getElementById(`slider`).style.transform = `translateX(0)`;
   }
 };
 
-export const previous = (k, setK) => {
-  if (k === 2) {
-    setK(1);
+export const previous = (k, setK, length) => {
+  if (k === 1) {
+    setK(0);
     document.getElementById(`card0`).style.opacity = `1`;
     document.getElementById(`card0`).style.transformOrigin = `right`;
-    document.getElementById(`card0`).style.transform = `rotateY(-0deg)`;
-    document.getElementById(`card1`).style.opacity = `0.4`;
-    document.getElementById(`card1`).style.transformOrigin = `left`;
-    document.getElementById(`card1`).style.transform = `rotateY(-90deg)`;
-    document.getElementById(`card2`).style.opacity = `0.4`;
-    document.getElementById(`slider`).style.transform = `translateX(0vw)`;
-  } else if (k === 3) {
-    setK(2);
-    document.getElementById(`card0`).style.opacity = `0.4`;
-    document.getElementById(`card1`).style.opacity = `1`;
-    document.getElementById(`card1`).style.transformOrigin = `right`;
-    document.getElementById(`card1`).style.transform = `rotateY(-0deg)`;
-    document.getElementById(`card2`).style.opacity = `0.4`;
-    document.getElementById(`card2`).style.transformOrigin = `left`;
-    document.getElementById(`card2`).style.transform = `rotateY(-90deg)`;
-    document.getElementById(`slider`).style.transform = `translateX(-100%)`;
+    document.getElementById(`card0`).style.transform = `rotateY(0deg)`;
+
+    for (let i = 1; i < length; i++) {
+      document.getElementById(`card${i}`).style.opacity = `0.4`;
+      document.getElementById(`card${i}`).style.transformOrigin = `left`;
+      document.getElementById(`card${i}`).style.transform = `rotateY(90deg)`;
+    }
+
+    document.getElementById(`slider`).style.transform = `translateX(0)`;
+  } else if (k > 1) {
+    setK(k - 1);
+    document.getElementById(`card${k - 2}`).style.opacity = `0.4`;
+    document.getElementById(`card${k - 1}`).style.opacity = `1`;
+    document.getElementById(`card${k - 1}`).style.transformOrigin = `right`;
+    document.getElementById(`card${k - 1}`).style.transform = `rotateY(-0deg)`;
+    document.getElementById(`card${k}`).style.opacity = `0.4`;
+    document.getElementById(`card${k}`).style.transformOrigin = `left`;
+    document.getElementById(`card${k}`).style.transform = `rotateY(-90deg)`;
+    document.getElementById(`slider`).style.transform = `translateX(-${100 * (k - 1)}%)`;
+
   } else {
-    setK(3);
-    document.getElementById(`card0`).style.opacity = `0.4`;
+    setK(0);
+    document.getElementById(`card0`).style.opacity = `1`;
     document.getElementById(`card0`).style.transformOrigin = `right`;
-    document.getElementById(`card0`).style.transform = `rotateY(90deg)`;
-    document.getElementById(`card1`).style.transformOrigin = `right`;
-    document.getElementById(`card1`).style.transform = `rotateY(90deg)`;
-    document.getElementById(`card1`).style.opacity = `0.4`;
-    document.getElementById(`card2`).style.opacity = `1`;
-    document.getElementById(`card2`).style.transformOrigin = `right`;
-    document.getElementById(`card2`).style.transform = `rotateY(0deg)`;
-    document.getElementById(`slider`).style.transform = `translateX(-200%)`;
+    document.getElementById(`card0`).style.transform = `rotateY(0deg)`;
+
+    for (let i = 1; i < length; i++) {
+      document.getElementById(`card${i}`).style.opacity = `0.4`;
+      document.getElementById(`card${i}`).style.transformOrigin = `left`;
+      document.getElementById(`card${i}`).style.transform = `rotateY(90deg)`;
+    }
+
+    document.getElementById(`slider`).style.transform = `translateX(0)`;
   }
 };
