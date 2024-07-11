831af5675d0ab8517d9eb6a651fda37e80776713
Gwerh <gwerh.dev@gmail.com>
2024-07-11 17:45:35 -0400

    styling sekeleton

--- src/app/components/Featured/EmptyCard.jsx
+++ src/app/components/Featured/EmptyCard.jsx
@@ -3,7 +3,12 @@ import s from './EmptyCard.module.css';
 export const EmptyCard = () => {
   return (
     <article className={s.container}>
-
+      <div className={s.image}></div>
+      <ul className={s.listContainer}>
+        <li className={s.title}></li>
+        <li className={s.subtitle}></li>
+        <li className={s.buttons}></li>
+      </ul>
     </article>
   )
 }
