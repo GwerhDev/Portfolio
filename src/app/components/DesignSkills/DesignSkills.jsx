61bcf31b33dbd2e8cd70fda9e9c3ccd180f9b911
Gwerh <gwerh.dev@gmail.com>
2024-07-30 16:44:04 -0400

    styling

--- src/app/components/DesignSkills/DesignSkills.jsx
+++ src/app/components/DesignSkills/DesignSkills.jsx
@@ -9,37 +9,34 @@ import rhinocerosSvg from '../../../assets/images/svg/rhinoceros-icon.svg';
 export const DesignSkills = (props) => {
   return (
     <div className={s.container}>
-      <section className="section-container">
-        <div className={s.skillsCont}>
-          <ul className={s.iconList}>
-            <li className={s.icon}>
-              <img className={s.svgIcons} src={photoshopSVG} alt="" />
-              <h5 style={{ color: 'white' }}>PhotoSHop</h5>
-            </li>
-            <li className={s.icon}>
-              <img className={s.svgIcons} src={illustratorSVG} alt="" />
-              <h5 style={{ color: 'white' }}>Illustrator</h5>
-            </li>
-            <li className={s.icon}>
-              <img className={s.svgIcons} src={premiereproSVG} alt="" />
-              <h5 style={{ color: 'white' }}>PremierePro</h5>
-            </li>
-            <li className={s.icon}>
-              <img className={s.svgIcons} src={aftereffectsSVG} alt="" />
-              <h5 style={{ color: 'white' }}>AfterEffects</h5>
-            </li>
-            <li className={s.icon}>
-              <img className={s.svgIcons} src={blenderSVG} alt="" />
-              <h5 style={{ color: 'white' }}>Blender</h5>
-            </li>
-            <li className={s.icon}>
-              <img className={s.svgIcons} src={rhinocerosSvg} alt="" />
-              <h5 style={{ color: 'white' }}>Rhinoceros</h5>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <div className={s.skillsCont}>
+        <ul className={s.iconList}>
+          <li className={s.icon}>
+            <img className={s.svgIcons} src={photoshopSVG} alt="" />
+            <h5 style={{ color: 'white' }}>PhotoSHop</h5>
+          </li>
+          <li className={s.icon}>
+            <img className={s.svgIcons} src={illustratorSVG} alt="" />
+            <h5 style={{ color: 'white' }}>Illustrator</h5>
+          </li>
+          <li className={s.icon}>
+            <img className={s.svgIcons} src={premiereproSVG} alt="" />
+            <h5 style={{ color: 'white' }}>PremierePro</h5>
+          </li>
+          <li className={s.icon}>
+            <img className={s.svgIcons} src={aftereffectsSVG} alt="" />
+            <h5 style={{ color: 'white' }}>AfterEffects</h5>
+          </li>
+          <li className={s.icon}>
+            <img className={s.svgIcons} src={blenderSVG} alt="" />
+            <h5 style={{ color: 'white' }}>Blender</h5>
+          </li>
+          <li className={s.icon}>
+            <img className={s.svgIcons} src={rhinocerosSvg} alt="" />
+            <h5 style={{ color: 'white' }}>Rhinoceros</h5>
+          </li>
+        </ul>
+      </div>
     </div>
-
   )
 }
