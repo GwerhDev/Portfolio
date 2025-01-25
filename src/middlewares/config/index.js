85a5139f598c6254f24a89c224571da6963d5542
Gwerh <gwerh.dev@gmail.com>
2025-01-25 17:30:26 -0300

    changing api for terminal core

--- src/middlewares/config/index.js
+++ src/middlewares/config/index.js
@@ -1,7 +1,7 @@
 import { environment } from "../../environment";
 import { DEVELOPMENT } from "../misc/consts";
 
-export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080/gwerh":"https://developers-terminalkiller.fly.dev/gwerh";
+export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080/gwerh":"https://developers-terminalcore.fly.dev/gwerh";
 export const URL_WIKI_API = "https://es.wikipedia.org/w/api.php";
 export const URL_DEVDAILYJOKE_API = "https://devdailyjoke-api.fly.dev";
 
