49e6e8d4de87de2751c67211007e0221a9d9b26c
gerardo <gerardo@recemed.com>
2025-10-23 09:46:17 -0300

    styling decorations

--- src/app/components/Utils/Separator/Separator.jsx
+++ src/app/components/Utils/Separator/Separator.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import separator from "../../../../assets/images/png/separator-image.png"
 
 export const Separator = (props) => {
-  const { marginTop, width } = props
+  const { marginTop, width } = props || {};
   return (
     <img className={s.separator} src={separator} alt="" width={width ?? "300"} style={{ marginTop }} />
   )
